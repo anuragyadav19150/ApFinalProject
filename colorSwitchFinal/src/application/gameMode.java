33c4855e885e7cccdc77cf837707485429e19934
anuragyadav19150 <55681370+anuragyadav19150@users.noreply.github.com>
2020-11-22 11:38:14 +0530

    Update gameMode.java
    only previously saved game button left

--- colorSwitchFinal/src/application/gameMode.java
+++ colorSwitchFinal/src/application/gameMode.java
@@ -2,6 +2,10 @@ package application;
 
 
 import javafx.application.Application;
+import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.stage.Stage; 
+import javafx.scene.shape.Rectangle;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.application.Application; 
@@ -49,104 +53,104 @@ public class gameMode {
 	}
 	
 	
-	public static Group doit(Stage s,Scene s3) {
-	//	final Scene s2 = null ;
+	public static Group doit(Stage s,Scene s3,Scene s1) {
+	
 		 Canvas canvas = new Canvas();
 		 canvas.setHeight(800); 
-	        canvas.setWidth(800); 
+	     canvas.setWidth(800); 
 	        
-	        classic c=new classic();
+	     classic c=new classic();
 	  
-	        // graphics context 
-	        GraphicsContext graphics_context =  
-	            canvas.getGraphicsContext2D(); 
-	        graphics_context.setFill(Color.LIGHTBLUE); 
-	        graphics_context.fillRect(0, 0, 600, 600);
-	  
-	        // set fill for rectangle 
+	       
+	     GraphicsContext graphics_context =  canvas.getGraphicsContext2D(); 
+
 	        graphics_context.setFill(Color.YELLOW); 
 	        graphics_context.fillRect(30, 30, 70, 70); 
 	  
-	        // set fill for rectangle 
 	        graphics_context.setFill(Color.RED); 
 	        graphics_context.fillRect(20, 20, 70, 70);
-	        
-	        
-	  
-	        // set fill for oval 
+	      
 	        graphics_context.setFill(Color.BLUE); 
 	        graphics_context.fillRect(10, 10, 70,70); 
 	  
+       
+	        StackPane sp = new StackPane();
+	        Image img = new Image("b8.jpg");
+	        ImageView imgView = new ImageView(img);
+	        imgView.setX(50); 
+	        imgView.setY(25); 
 	        
+	      
+	        imgView.setFitHeight(800); 
+	        imgView.setFitWidth(800);
+	        sp.getChildren().add(imgView); 
 	        
 	        GraphicsContext gc = canvas.getGraphicsContext2D();
 	        GraphicsContext gc1 = canvas.getGraphicsContext2D();
 	         
-	        gc.setFill( Color.RED );
-	        gc.setStroke( Color.BLACK );
+	        gc.setFill( Color.HOTPINK );
+	        gc.setStroke( Color.HOTPINK );
 	        gc.setLineWidth(2);
-	        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 28 );
+	        Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
 	        gc.setFont( theFont );
-	        gc.fillText( "CHOOSE GAME_MODE!", 120, 250  );
-	        gc.strokeText( "CHOOSE GAME_MODE!", 120, 250  );
+	        gc.fillText( "CHOOSE GAME_MODE!", 150, 250  );
+	        gc.strokeText( "CHOOSE GAME_MODE!", 150, 250  );
 
 	        
-	        gc1.setFill( Color.BLUE );
-	        gc1.setStroke( Color.BLACK );
+	        gc1.setFill( Color.CYAN );
+	        gc1.setStroke( Color.CYAN );
 	        gc1.setLineWidth(1.5);
 	        Font theFont1 = Font.font( "Times New Roman", FontWeight.BOLD, 50 );
 	        gc1.setFont( theFont1 );
-	        gc1.fillText( "COLOR SWITCH", 80, 150);
-	        gc1.strokeText( "COLOR SWITCH", 80, 150 );
-	      //  FillTransition fill = new FillTransition();  
-	       // Circle cir = new Circle(400,450,100);
+	        gc1.fillText( "COLOR SWITCH", 200, 150);
+	        gc1.strokeText( "COLOR SWITCH", 200, 150 );
+	      
 	        
 	        Button b = new Button("CLASSIC MODE START"); 
 	        Button bt = new Button("PREVIOUS GAMES"); 
-	        Button btt = new Button("    EXIT   "); 
+	        Button btt = new Button(".     EXIT     ."); 
 	        
-	        // create a stack pane 
+	       
 	        Pane r = new Pane(); 
 	  
-	        // create a label 
-	        Label l = new Label("");
+	       
 	        b.setScaleY(2);
 	        b.setScaleX(2);
-	        b.setLayoutX(100);  //this is how you change locations
-	        b.setLayoutY(350);
+	        b.setLayoutX(200);  
+	        b.setLayoutY(450);
 	        
 	        bt.setScaleY(2);
 	        bt.setScaleX(2);
-	        bt.setLayoutX(400);  //this is how you change locations
-	        bt.setLayoutY(350);
+	        bt.setLayoutX(500);  
+	        bt.setLayoutY(450);
 	        r.getChildren().add(b); 
-	        // bt.setOnAction(event);
+	      
 	        
 	        btt.setScaleY(2);
 	        btt.setScaleX(2);
-	        btt.setLayoutX(250);  //this is how you change locations
-	        btt.setLayoutY(450);
+	        btt.setLayoutX(375);  
+	        btt.setLayoutY(550);
 	        
 	   	  
-	         // add button 
+	        
 	         r.getChildren().add(bt);
 	         r.getChildren().add(btt);
 	         
 	         
-	       //  s1=new Scene(null,600,600);
+	       
 	         Group f1=new Group();
 	         
 	         b.setOnAction(e -> {
 	         	s.setScene(s2);
 	         	//g.doit(s1);
 	         	
-	         	f1.getChildren().add(c.doit(s));
+	         	f1.getChildren().add(c.doit(s,s1));
 	                  	
 	     		
 	         });
-	         s2=new Scene(f1,600,600);
+	         s2=new Scene(f1,800,800);
 	         
-	         //StackPane sp=new StackPane();
+	         
 	         
 	         
 	         btt.setOnAction(e -> {
@@ -154,9 +158,54 @@ public class gameMode {
 	                  	
 	     		
 	         });
+	         
+	         
+	         
+	         Rectangle rectangle = new Rectangle();  
+	         rectangle.setX(0); 
+	         rectangle.setY(650); 
+	         rectangle.setWidth(800); 
+	         rectangle.setHeight(10); FillTransition fill = new FillTransition();
+	         fill.setAutoReverse(true);
+	         fill.setCycleCount(50);
+	         fill.setDuration(Duration.millis(2000));
+	         fill.setFromValue(Color.RED);
+	         fill.setToValue(Color.CYAN); 
+	         fill.setShape(rectangle);
+	         fill.play(); 
+	         
+	         
+	         Rectangle r1 = new Rectangle();  
+	         r1.setX(0); 
+	         r1.setY(700); 
+	         r1.setWidth(800); 
+	         r1.setHeight(10); 
+	         FillTransition fill1 = new FillTransition();  
+	         fill1.setAutoReverse(true);
+	         fill1.setCycleCount(50);  
+	         fill1.setDuration(Duration.millis(2000));  
+	         fill1.setFromValue(Color.RED);  
+	         fill1.setToValue(Color.CYAN); 
+	         fill1.setShape(r1);
+	         fill1.play(); 
+	         
+	         
+	         Rectangle r2 = new Rectangle();
+	         r2.setX(0); 
+	         r2.setY(750); 
+	         r2.setWidth(800); 
+	         r2.setHeight(10); 
+	         FillTransition fill2 = new FillTransition();  
+	         fill2.setAutoReverse(true);  
+	         fill2.setCycleCount(50);  
+	         fill2.setDuration(Duration.millis(2000));  
+	         fill2.setFromValue(Color.RED);  
+	         fill2.setToValue(Color.CYAN); 
+	         fill2.setShape(r2);
+	         fill2.play(); 
 	  
 	        
-	        Group g2=new Group(canvas,r);
+	        Group g2=new Group(sp,canvas,r,rectangle,r1,r2);
 	        return g2;
 		
 	}
